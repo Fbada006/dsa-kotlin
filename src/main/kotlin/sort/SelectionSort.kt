4eea41d228cc0de8823742f628097f8ec911aab3
Ferdinand Bada <ferdinandkamata@duck.com>
2024-09-09 13:46:14 +0300

    insertion sort comment update

--- src/main/kotlin/sort/SelectionSort.kt
+++ src/main/kotlin/sort/SelectionSort.kt
@@ -4,8 +4,7 @@ fun selectionSort(array: IntArray) {
     for (i in array.indices) {
         // Track the minimum index
         var min = i
-        // Start from i + 1 because we need to ignore the min above, which we assume is our minimum
-        //to make this more efficient
+        // Start from i + 1 because we need to ignore the item before i
         for (j in i + 1 until array.size) {
             if (array[j] < array[min]) {
                 // Find the minimum value
