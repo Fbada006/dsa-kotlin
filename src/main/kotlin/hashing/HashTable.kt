65d12c7f626f90ace83feefe28148808e51bb8c1
Ferdinand Bada <ferdinandkamata@duck.com>
2024-09-17 17:27:51 +0300

    implement put of a hash map

--- src/main/kotlin/hashing/HashTable.kt
+++ src/main/kotlin/hashing/HashTable.kt
@@ -7,7 +7,37 @@ class HashTable(capacity: Int) {
     fun getSize() = size
     fun isEmpty() = size == 0
 
-    fun put(key: Int, value: Int) {}
+    fun put(key: Int?, value: String?) {
+        require(value != null) { "Value cannot be null" }
+        require(key != null) { "Key cannot be null" }
+
+        val bucketIndex = getBucketIndex(key)
+        var head = buckets[bucketIndex]
+
+        // First traverse the list to check if there are any duplicate keys. If any, simply replace the value of the key
+        // and return. A map cannot have more than one similar key
+        while (head != null) {
+            if (key == head.key) {
+                head.value = value
+                return
+            }
+            head = head.next
+        }
+
+        size++
+        // move the head back to the start
+        head = buckets[bucketIndex]
+        val newNode = HashNode(key, value)
+        // Join the nodes by inserting the new node before the current head
+        newNode.next = head
+        // the new node comes to the very first item of the bucket
+        buckets[bucketIndex] = newNode
+    }
+
+    // We use modular (%) hashing
+    private fun getBucketIndex(key: Int): Int {
+        return key % buckets.size
+    }
 
     fun get(key: Int): String? {
         return null
@@ -20,6 +50,16 @@ class HashTable(capacity: Int) {
 
 data class HashNode(
     val key: Int,
-    val value: String,
-    val next: HashNode?
+    var value: String,
+    var next: HashNode? = null
 )
+
+
+fun main() {
+    val table = HashTable(10)
+    table.put(105, "Tom")
+    table.put(21, "John")
+    table.put(21, "New John")
+
+    println(table.getSize()) // Is 2
+}
